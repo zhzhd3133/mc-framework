8105022b335c5d54c15b9d6915256f8c92dff1df
zhzhd3133@163.com <zhzhd3133@163.com>
2017-03-16 21:49:44 +0800

    初步建立

--- mmc-core/src/main/java/MMCMain.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by master on 2017/3/7.
- */
-public class MMCMain {
-    public static void main(String[] args){
-        System.out.println(":: ----- zzd.com ------ ::  mmc main");
-    }
-}
