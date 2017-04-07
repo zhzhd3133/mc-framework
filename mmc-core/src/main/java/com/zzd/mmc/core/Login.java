2c4cabaf855614beeb08b56f4e2d5bed9dd0639c
zhzhd3133 <zzd423133>
2017-04-07 17:31:05 +0800

    spring-mybati-druid

--- mmc-core/src/main/java/com/zzd/mmc/core/Login.java
+++ mmc-core/src/main/java/com/zzd/mmc/core/Login.java
@@ -3,7 +3,6 @@ package com.zzd.mmc.core;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
 import java.util.HashMap;
 import java.util.Map;
 
