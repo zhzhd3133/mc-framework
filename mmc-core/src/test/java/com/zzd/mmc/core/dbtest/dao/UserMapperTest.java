c8ef6414a7eec1754c7597c927aaf235dd809b8d
zhzhd3133 <zzd423133>
2017-06-09 09:35:44 +0800

    spring-mybati-druid

--- mmc-core/src/test/java/com/zzd/mmc/core/dbtest/dao/UserMapperTest.java
+++ mmc-core/src/test/java/com/zzd/mmc/core/dbtest/dao/UserMapperTest.java
@@ -2,6 +2,7 @@ package com.zzd.mmc.core.dbtest.dao;
 
 import com.zzd.mmc.core.dbtest.bean.User;
 import org.junit.Test;
+import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,7 +15,7 @@ public class UserMapperTest extends SpringTest{
     UserMapper userMapper;
     @Test
     public void testSelectUsers() throws Exception {
-        User u = userMapper.selectUsers("liuqirong");
+        User u = userMapper.selectUsers("yangliuqing");
         System.out.println(u);
     }
 
