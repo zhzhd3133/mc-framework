28ba8cab8dcc617fad53d5d14405c0aae3900417
zhzhd3133 <zzd423133>
2017-06-09 09:27:52 +0800

    spring-mybati-druid

--- mmc-core/src/main/java/com/zzd/mmc/core/holder/PropertiesHolder.java
+++ mmc-core/src/main/java/com/zzd/mmc/core/holder/PropertiesHolder.java
@@ -1,20 +1,30 @@
 package com.zzd.mmc.core.holder;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
-
-import java.util.Properties;
+import java.util.*;
 
 /**
  * Created by master-pc on 2017/4/5.
  */
 public class PropertiesHolder extends PropertyPlaceholderConfigurer {
+     Logger log = LoggerFactory.getLogger(PropertiesHolder.class);
     private static  Properties appProperties;
     @Override
     protected void processProperties(ConfigurableListableBeanFactory beanFactoryToProcess, Properties props) throws BeansException {
         super.processProperties(beanFactoryToProcess, props);
-        System.out.println("fsdfsdfsdfsdfsd");
+        Set<Map.Entry<Object,Object>> set = props.entrySet();
+        log.info("Properties ::: 读取配置信息如下（KEY == VALUE）"  );
+        Iterator<Map.Entry<Object, Object>> it = set.iterator();
+        while(it.hasNext()) {
+            Map.Entry<Object, Object> enter = it.next();
+            if(log.isDebugEnabled()){
+                log.debug("Entry ::: " + enter.getKey() + " == " + enter.getValue());
+            }
+        }
         appProperties= props;
     }
 
